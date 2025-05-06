bd64f92f89cb2dcaaeb0e3a1c706ec571b0adc10
Wafair <119963003+Wafair@users.noreply.github.com>
2025-05-06 12:20:37 +0700

    Recovered utils/protocol.py from stash after merge conflict

---
+++ utils/protocol.py
@@ -0,0 +1,110 @@
+import json
+from enum import Enum
+
+class Command(Enum):
+    LIST = "LIST"
+    HOST = "HOST"
+    MESSAGE = "MESSAGE"
+    BROADCAST = "BROADCAST"
+    SIGNIN = "SIGNIN"
+    SIGNUP = "SIGNUP"
+    GUEST = "GUEST"
+    
+class Status(Enum):
+    OK = "OK"
+    REQUEST_ERROR = "REQUEST_ERROR"
+    SERVER_ERROR = "SERVER_ERROR"
+    
+def create_request(command, payload, separator = ""):
+    """
+    Create a request string to be sent to the tracker or peer.
+    
+    Args:
+        command (Command): The command to be executed.
+        payload (dict): The data associated with the command.
+        separator (string): Separate symbol to split 2 different response 
+
+    Returns:
+        str: The formatted request string.
+    """
+    # Switch case for command
+    if command == Command.LIST:
+        return f"{command.value}\r\n".encode("utf-8")
+    elif command == Command.HOST:
+        return f"{command.value}\r\n{json.dumps(payload) + separator}".encode("utf-8")
+    elif command == Command.MESSAGE:
+        return f"{command.value}\r\n{json.dumps(payload) + separator}".encode("utf-8")
+    elif command == Command.BROADCAST:
+        return f"{command.value}\r\n{json.dumps(payload) + separator}".encode("utf-8")
+    elif command == Command.SIGNIN:
+        return f"{command.value}\r\n{json.dumps(payload) + separator}".encode("utf-8")
+    elif command == Command.SIGNUP:
+        return f"{command.value}\r\n{json.dumps(payload) + separator}".encode("utf-8")
+    elif command == Command.GUEST:
+        return f"{command.value}\r\n{json.dumps(payload) + separator}".encode("utf-8")
+    
+    
+def parse_request(response, isSeparated = False):
+    """
+    Parse the response string from the tracker or peer.
+    
+    Args:
+        response (str): The response string to be parsed.
+        isSeparated (bool): False: not using separater => response hasn't been decode
+                            True: using separater => response has been decoded
+
+    Returns:
+        dict: The parsed response data.
+    """
+    try:
+        if not isSeparated:
+            response = response.decode("utf-8")
+        command, payload = response.split("\r\n", 1)
+        if payload:
+            payload = json.loads(payload)
+        else:
+            payload = {}
+        return command, payload
+    except ValueError:
+        print(f"Error parsing response: {response}")
+        raise ValueError("Invalid response format")
+    
+def create_response(status, payload, separator = ""):
+    """
+    Create a response string to be sent back to the client.
+    
+    Args:
+        status (Status): The status of the response.
+        payload (dict): The data associated with the response.
+        separator (string): Separate symbol to split 2 different response 
+    Returns:
+        str: The formatted response string.
+    """
+    return f"{status.value}\r\n{json.dumps(payload) + separator}".encode("utf-8")
+
+def parse_response(response, isSeparated = False):
+    """
+    Parse the response string from the tracker or peer.
+    
+    Args:
+        response (str): The response string to be parsed.
+        isSeparated (bool): False: not using separater => response hasn't been decode
+                            True: using separater => response has been decoded
+    Returns:
+        str: The status of the response.
+        dict: The parsed response data.
+    """
+    try:
+        if not isSeparated:
+            response = response.decode("utf-8")
+
+        status, payload = response.split("\r\n", 1)
+        if payload:
+            payload = json.loads(payload)
+        else:
+            payload = {}
+        return status, payload
+    except ValueError:
+        print(f"Error parsing response: {response}")
+        raise ValueError("Invalid response format")
+
