cc2b3f29bf57a009a12e396d430b35c46309fffb
Wafair <119963003+Wafair@users.noreply.github.com>
2025-05-06 16:02:59 +0700

    push changing in tracker

--- tracker.py
+++ tracker.py
@@ -1,38 +1,216 @@
 import socket
-from threading import Thread
+import copy
+from threading import Thread, Lock
 import json
 from utils.protocol import create_response, parse_request, Status
 
-def handle_user_submission(addr, conn):
+separater = '\t'
+lock = Lock()
+account_list = [
+    {
+        "username":"phuongthao",
+        "password":"15052004",
+    },
+    {
+        "username":"thuytien",
+        "password":"04032004",
+    },
+    {
+        "username":"minhhieu",
+        "password":"07042004",
+    },
+    {
+        "username":"diennguyen",
+        "password":"12345678",
+    },
+    {
+        "username":"thanhtam",
+        "password":"987654",
+    },
+    {
+        "username":"tienphat",
+        "password":"0246810",
+    }
+]
+
+visitor_list = [
+
+]
+
+channel_list = [
+    {
+        "channel_name": "MusicZone",
+        "ip": "192.168.1.10",
+        "port": 2222
+    },
+    {
+        "channel_name": "GameChat",
+        "ip": "192.168.1.11",
+        "port": 2223
+    }
+]
+
+
+def get_list(conn, lock):    
+    #TODO: REQUEST: GETLIST: send the channel list 
+    global channel_list 
     try:
-        data = conn.recv(4096)
-        if not data:
-            print("No data received from", addr)
+        with lock:
+            safe_copy = copy.deepcopy(channel_list)
+        response = create_response(Status.OK, safe_copy, separater)
+        conn.sendall(response)
+
+    except Exception as e:
+        error_msg = create_response(Status.SERVER_ERROR, {"message": str(e)})
+
+        try:
+            conn.sendall(error_msg)
+        except:
+            pass 
+
+        print(f"[Error] Failed to send: {e}")
+
+def visitor(conn, lock, payload):
+    global account_list
+    try:
+        with lock:
+            if any(account['username'] == payload['username'] for account in account_list):
+                message = {"Message": "Username is existed"}
+                status = Status.REQUEST_ERROR
+            else:    
+                visitor_list.append(
+                    {
+                        "username": payload['username']
+                    }
+                )
+                message = {"message": f"Create visitor account {payload['username']} successful"}
+                status = Status.OK
+        
+        response = create_response(status, message, separater) 
+        conn.sendall(response)
+
+    except Exception as e:
+        error_msg = create_response(Status.SERVER_ERROR, {"message": str(e)})
+        try:
+            conn.sendall(error_msg)
+        except:
+            pass 
+
+        print(f"[Error] Failed to send: {e}")
+    
+
+def authenticate_user(conn, lock, payload):
+    global account_list
+    message = {"message": "Invalid username or password"}
+    status = Status.REQUEST_ERROR
+
+    try:
+        with lock:
+            for account in account_list:
+                if account['username'] == payload['username'] and account['password'] == payload['password']:
+                    message = {"message": "Login successful"}
+                    status = Status.OK
+                    break
+        
+        response = create_response(status, message, separater) 
+        conn.sendall(response)
+
+    except Exception as e:
+        error_msg = create_response(Status.SERVER_ERROR, {"message": str(e)})
+        try:
+            conn.sendall(error_msg)
+        except:
+            pass 
+
+        print(f"[Error] Failed to send: {e}")    
+    
+    
+
+def create_account(conn, lock, payload):
+    #TODO: REQUEST: CREATEACC: check duplicate username, then add to the account_list
+    global account_list
+    username = payload.get("username")
+    password = payload.get("password")
+
+    try:
+        with lock:
+            if any(acc["username"] == username for acc in account_list):
+                message = {"message": "Username already been used"}
+                status = Status.REQUEST_ERROR 
+            else:    
+                account_list.append(
+                    {
+                        "username": username,
+                        "password": password
+                    }
+                )
+                message = {"message": f"Create user account {username} successful"}
+                status = Status.OK
+
+        response = create_response(status, message, separater) 
+        conn.sendall(response)
+
+    except Exception as e:
+        error_msg = create_response(Status.SERVER_ERROR, {"message": str(e)})
+        try:
+            conn.sendall(error_msg)
+        except:
+            pass 
+
+        print(f"[Error] Failed to send: {e}")  
+
+
+
+
+
+def handle_user_submission(addr, conn):
+    global lock   
+
+    
+    ## Handle user request (while loop for listening)##
+    buffer = ""
+
+    while True:
+        try:
+            data = conn.recv(4096)
+            if not data:
+                print("No data received from", addr)
+                return
+            buffer += data.decode("utf-8")
+
+            while separater in buffer:
+                line, buffer = buffer.split(separater,1)
+                command, payload = parse_request(line, isSeparated=True)
+                
+                if command == "LIST":
+                    Thread(target=get_list, arg=(conn, lock), daemon=True).start()
+                    
+                elif command == "HOST":
+                    # peers.append({
+                    #     "channel_name": payload['channel_name'],
+                    #     "peer_server_ip": payload['peer_server_ip'],
+                    #     "peer_server_port": payload['peer_server_port'],
+                    # })
+                    # response = create_response(Status.OK, {
+                    #     "status": "success",
+                    #     "channel_name": payload['channel_name'],
+                    # })
+                    # conn.sendall(response)
+                    pass
+                elif command == "SIGNIN":
+                    Thread(target=authenticate_user, args=(conn, lock, payload), daemon=True).start()
+                elif command == "SIGNUP":
+                    Thread(target=create_account, args=(conn, lock, payload), daemon=True).start()
+                elif command == "GUEST":
+                    Thread(target=visitor, args=(conn, lock, payload), daemon=True).start()
+                else:
+                    pass
+            
+        except ValueError as e:
+            print("Error parsing data:", e)
             return
         
-        command, payload = parse_request(data)
-        
-        if command == "LIST":
-            response = create_response(Status.OK, peers)
-            conn.send(response)
-        elif command == "HOST":
-            # A host can have multiple channels
-            for peer in payload:
-                peers.append(peer)
-                
-            response = create_response(Status.OK, {
-                "status": "success",
-                "channel_name": [peer["channel_name"] for peer in payload],
-            })
-            conn.send(response)
-        elif command == "MESSAGE":
-            pass
-        
-    except ValueError as e:
-        print("Error parsing data:", e)
-        return
-    finally:
-        conn.close()
+    conn.close()
 
 def listen(ip, port):
     try:
@@ -54,7 +232,6 @@ def listen(ip, port):
 if __name__ == "__main__":
     
     # Dummy values for testing
-    peers = []
     ip = '127.0.0.1'
     port = 22236
     listen(ip, port)
--- utils/protocol.py
+++ utils/protocol.py
@@ -7,55 +7,69 @@ class Command(Enum):
     MESSAGE = "MESSAGE"
     BROADCAST = "BROADCAST"
     CACHE = "CACHE"
+    SIGNIN = "SIGNIN"
+    SIGNUP = "SIGNUP"
+    GUEST = "GUEST"
     
 class Status(Enum):
     OK = "OK"
     REQUEST_ERROR = "REQUEST_ERROR"
     SERVER_ERROR = "SERVER_ERROR"
     
-def create_request(command, payload):
+def create_request(command, payload, separater = ""):
     """
     Create a request string to be sent to the tracker or peer.
     
     Args:
         command (Command): The command to be executed.
         payload (dict): The data associated with the command.
-        
+        separator (string): Separate symbol to split 2 different response 
+    
     Returns:
         str: The formatted request string.
     """
     # Switch case for command
     if command == Command.LIST:
-        return f"{command.value}\r\n".encode("utf-8")
-    elif command == Command.HOST:
+        return f"{command.value}\r\n{separater}".encode("utf-8")
+    # elif command == Command.HOST:
+    #     # If the payload is a dictionary, convert it to a list of dictionaries
+    #     if isinstance(payload, dict):
+    #         payload = [payload]
+    #     return f"{command.value}\r\n{json.dumps(payload)}{Separater}".encode("utf-8")
+    # elif command == Command.MESSAGE:
+    #     # If the payload is a dictionary, convert it to a list of dictionaries
+    #     if isinstance(payload, dict):
+    #         payload = [payload]
+    #     return f"{command.value}\r\n{json.dumps(payload)}{Separater}".encode("utf-8")
+    # elif command == Command.CACHE:
+    #     # If the payload is a dictionary, convert it to a list of dictionaries
+    #     if isinstance(payload, dict):
+    #         payload = [payload]
+    #     return f"{command.value}\r\n{json.dumps(payload)}{Separater}".encode("utf-8")
+    elif command in [Command.HOST, Command.MESSAGE,
+                     Command.CACHE, Command.BROADCAST,
+                     Command.SIGNIN, Command.SIGNUP, Command.GUEST]:
         # If the payload is a dictionary, convert it to a list of dictionaries
         if isinstance(payload, dict):
             payload = [payload]
-        return f"{command.value}\r\n{json.dumps(payload)}".encode("utf-8")
-    elif command == Command.MESSAGE:
-        # If the payload is a dictionary, convert it to a list of dictionaries
-        if isinstance(payload, dict):
-            payload = [payload]
-        return f"{command.value}\r\n{json.dumps(payload)}".encode("utf-8")
-    elif command == Command.CACHE:
-        # If the payload is a dictionary, convert it to a list of dictionaries
-        if isinstance(payload, dict):
-            payload = [payload]
-        return f"{command.value}\r\n{json.dumps(payload)}".encode("utf-8")
-    
+        return f"{command.value}\r\n{json.dumps(payload)}{separater}".encode("utf-8")
 
-def parse_request(response):
+
+def parse_request(response, isSeparated = False):
     """
     Parse the response string from the tracker or peer.
     
     Args:
         response (str): The response string to be parsed.
-        
+        isSeparated (bool): False: not using separater => response hasn't been decode
+                            True: using separater => response has been decoded
+
     Returns:
         dict: The parsed response data.
     """
     try:
-        response = response.decode("utf-8")
+        if not isSeparated:
+            response = response.decode("utf-8")
         command, payload = response.split("\r\n", 1)
         if payload:
             payload = json.loads(payload)
@@ -66,32 +80,36 @@ def parse_request(response):
         print(f"Error parsing response: {response}")
         raise ValueError("Invalid response format")
     
-def create_response(status, payload):
+def create_response(status, payload, separater = ""):
     """
     Create a response string to be sent back to the client.
     
     Args:
         status (Status): The status of the response.
         payload (dict): The data associated with the response.
-        
+        separator (string): Separate symbol to split 2 different response 
+
     Returns:
         str: The formatted response string.
     """
-    return f"{status.value}\r\n{json.dumps(payload)}".encode("utf-8")
+    return f"{status.value}\r\n{json.dumps(payload)}{separater}".encode("utf-8")
 
-def parse_response(response):
+def parse_response(response, isSeparated = False):
     """
     Parse the response string from the tracker or peer.
     
     Args:
         response (str): The response string to be parsed.
-    
+        isSeparated (bool): False: not using separater => response hasn't been decode
+                            True: using separater => response has been decoded
+
     Returns:
         str: The status of the response.
         dict: The parsed response data.
     """
     try:
-        response = response.decode("utf-8")
+        if not isSeparated:
+            response = response.decode("utf-8")
         status, payload = response.split("\r\n", 1)
         if payload:
             payload = json.loads(payload)
